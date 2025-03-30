57ea78044188cae8d57e2bb075f2ad385084fdcf
Apurba Kumar Show <156194221+IITApurba@users.noreply.github.com>
2025-03-30 12:32:40 +0530

    Time: 0 ms (100%), Space: 11.2 MB (28.92%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> cur_perm;
+        vector<vector<int>> all_sol;
+        map<int, int> mp;
+        int n = nums.size();
+
+        for (int num : nums)
+            mp[num]++;
+
+        function<void(int)> rec = [&](int level) {
+            if (level == n) {
+                all_sol.push_back(cur_perm);
+                return;
+            }
+
+            for (auto& v : mp) {
+                if (v.second != 0) {
+                    v.second--;
+                    cur_perm.push_back(v.first);
+                    rec(level + 1);
+                    cur_perm.pop_back();
+                    v.second++;
+                }
+            }
+        };
+
+        rec(0);
+        
+        return all_sol;
+    }
+};
