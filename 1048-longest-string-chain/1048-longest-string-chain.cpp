115c663d87e97d1a9cafacc733373dcba2ae1ceb
Apurba Kumar Show <156194221+IITApurba@users.noreply.github.com>
2026-01-03 22:19:43 +0530

    Time: 47 ms (55.78%), Space: 18.6 MB (85.53%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
+    //pred(a,b) -> check is 'a' predecessor of 'b'
     bool pred(const string &a, const string &b) {
+        // len of b must be 1+a
         if (b.size() != a.size() + 1) return false;
+
+        // using 2 pointers to check b can be made by just using 1 char
         int i = 0, j = 0;
         bool used = false;
-        while (i < a.size() && j < (int)b.size()) {
+        while (i < a.size() && j < b.size()) {
             if (a[i] == b[j]) {
                 i++; j++;
             } else {
@@ -17,21 +21,28 @@ public:
     }
 
     int longestStrChain(vector<string>& words) {
-        int n = (int)words.size();
+        int n = words.size();
+
+        // sort the words array by length :p
         sort(words.begin(), words.end(), [&](const string &x, const string &y){
             if (x.size() != y.size()) return x.size() < y.size();
             return x < y;
         });
 
+        // dp = -1
         vector<int> dp(n, -1);
 
-        function<int(int)> rec = [&](int i) -> int {
+        // dp[i] -> longest word chain ending at i 
 
+        function<int(int)> rec = [&](int i) -> int {
+            
+            //cache check
             if (dp[i] != -1) return dp[i];
 
             int ans = 1;
             for (int j = 0; j < i; j++) {
-                if (words[j].size() + 1 == words[i].size() && pred(words[j], words[i])) {
+                // words[j] is a valid predecessor of words[i]
+                if (pred(words[j], words[i])) {
                     ans = max(ans, rec(j) + 1);
                 }
             }
@@ -39,7 +50,9 @@ public:
         };
 
         int ans = 0;
+
         for (int i = 0; i < n; i++) ans = max(ans, rec(i));
+        
         return ans;
     }
 };
