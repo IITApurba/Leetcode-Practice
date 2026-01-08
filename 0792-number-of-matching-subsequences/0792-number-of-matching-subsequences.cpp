1d0edc5a70f0abb4055463bebb5f3ff37561f444
Apurba Kumar Show <156194221+IITApurba@users.noreply.github.com>
2026-01-08 18:28:08 +0530

    Time: 527 ms (20.24%), Space: 411.5 MB (6.19%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
@@ -1,9 +1,6 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    vector<array<int, 26>> nx;
+    vector<vector<int>> nx;
     int n;
 
     bool rec(int pos, int j, const string& w) {
@@ -11,19 +8,21 @@ public:
             return true;
         if (pos > n)
             return false;
+
         int c = w[j] - 'a';
         int np = nx[pos][c];
         if (np == -1)
             return false;
+
         return rec(np + 1, j + 1, w);
     }
 
     int numMatchingSubseq(string s, vector<string>& words) {
         n = (int)s.size();
 
-        nx.assign(n + 1, {});
-        for (int c = 0; c < 26; c++)
-            nx[n][c] = -1;
+        // nx[pos][c] = next index >= pos with character c
+        nx.assign(n + 1, vector<int>(26, -1));
+
         for (int i = n - 1; i >= 0; i--) {
             nx[i] = nx[i + 1];
             nx[i][s[i] - 'a'] = i;
