2a873dd00e2189fe2d78ea39aab70f572f612b5c
Apurba Kumar Show <156194221+IITApurba@users.noreply.github.com>
2026-01-08 18:26:16 +0530

    Time: 155 ms (34.94%), Space: 227.1 MB (23.38%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
@@ -1,35 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
+    vector<array<int, 26>> nx;
     int n;
-    string s;
-    vector<vector<int>> nx; // nx[pos][c]
 
-    int rec(int pos, int j, const string& w) {
+    bool rec(int pos, int j, const string& w) {
         if (j == (int)w.size())
-            return 1;
+            return true;
         if (pos > n)
-            return 0;
-
+            return false;
         int c = w[j] - 'a';
         int np = nx[pos][c];
         if (np == -1)
-            return 0;
-
+            return false;
         return rec(np + 1, j + 1, w);
     }
 
-    int numMatchingSubseq(string S, vector<string>& words) {
-        s = S;
+    int numMatchingSubseq(string s, vector<string>& words) {
         n = (int)s.size();
 
-        // build nx[pos][c]
-        nx.assign(n + 1, vector<int>(26, -1));
+        nx.assign(n + 1, {});
+        for (int c = 0; c < 26; c++)
+            nx[n][c] = -1;
         for (int i = n - 1; i >= 0; i--) {
             nx[i] = nx[i + 1];
             nx[i][s[i] - 'a'] = i;
         }
 
-        // compress duplicates
         unordered_map<string, int> mp;
         mp.reserve(words.size() * 2);
         for (auto& w : words)
@@ -37,7 +36,8 @@ public:
 
         int ans = 0;
         for (auto& it : mp) {
-            ans += it.second * rec(0, 0, it.first);
+            if (rec(0, 0, it.first))
+                ans += it.second;
         }
         return ans;
     }
