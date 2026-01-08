ae16540a7accb7e6d40d7bf389561d41a69edcf7
Apurba Kumar Show <156194221+IITApurba@users.noreply.github.com>
2026-01-08 17:56:34 +0530

    Time: 549 ms (19.04%), Space: 413.8 MB (6.19%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int n;
     string s;
-    vector<array<int,26>> nxt;
+    vector<vector<int>> nx; // nx[pos][c]
 
-    // recursive check: can we match w[j..] starting from position pos in s?
     int rec(int pos, int j, const string &w) {
-        // base case
         if (j == (int)w.size()) return 1;
         if (pos > n) return 0;
 
         int c = w[j] - 'a';
-        int np = nxt[pos][c];
+        int np = nx[pos][c];
         if (np == -1) return 0;
 
         return rec(np + 1, j + 1, w);
@@ -21,12 +19,11 @@ public:
         s = S;
         n = (int)s.size();
 
-        // build nxt[pos][c]
-        nxt.assign(n + 1, {});
-        for (int c = 0; c < 26; c++) nxt[n][c] = -1;
+        // build nx[pos][c]
+        nx.assign(n + 1, vector<int>(26, -1));
         for (int i = n - 1; i >= 0; i--) {
-            nxt[i] = nxt[i + 1];
-            nxt[i][s[i] - 'a'] = i;
+            nx[i] = nx[i + 1];
+            nx[i][s[i] - 'a'] = i;
         }
 
         // compress duplicates
@@ -36,9 +33,7 @@ public:
 
         int ans = 0;
         for (auto &it : mp) {
-            const string &w = it.first;
-            int cnt = it.second;
-            ans += cnt * rec(0, 0, w);
+            ans += it.second * rec(0, 0, it.first);
         }
         return ans;
     }
