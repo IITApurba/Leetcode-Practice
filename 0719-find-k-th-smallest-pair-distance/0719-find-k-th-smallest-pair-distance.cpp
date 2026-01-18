d4c465b8d0a6411e4707fa6a99fb0088fae3b600
Apurba Kumar Show <156194221+IITApurba@users.noreply.github.com>
2026-01-18 15:40:30 +0530

    Time: 4 ms (42.76%), Space: 14.1 MB (35.51%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,56 +1,30 @@
 class Solution {
 public:
-    // The Check Function:
-    // Returns true if there are at least 'k' pairs with distance <= mid_dist
-    bool check(vector<int>& nums, int k, int mid_dist) {
-        int count = 0;
-        int left = 0;
-        int n = nums.size();
+    bool check(vector<int>& nums, int k, int mid) {
         
-        // Sliding Window approach to count pairs in O(N)
+        int count = 0, left = 0, n = nums.size();
+
         for (int right = 0; right < n; right++) {
-            // While the window is invalid (distance > mid_dist), shrink from left
-            while (nums[right] - nums[left] > mid_dist) {
+            while (nums[right] - nums[left] > mid)
                 left++;
-            }
-            // All indices between left and right-1 form a valid pair with 'right'
-            // specifically: (left, right), (left+1, right), ... (right-1, right)
-            count += (right - left);
+            count += right - left;
         }
-        
         return count >= k;
     }
 
     int smallestDistancePair(vector<int>& nums, int k) {
-        // 1. Sorting is mandatory for the Sliding Window logic to work
         sort(nums.begin(), nums.end());
-        
-        int n = nums.size();
-        
-        // 2. Define Search Space
-        // Smallest possible distance is 0
-        // Largest possible distance is between the max and min elements
-        int lo = 0;
-        int hi = nums[n-1] - nums[0];
-        int ans = hi;
+        int lo = 0, hi = nums.back() - nums.front(), ans = hi;
 
-        // 3. Binary Search Template (Minimization)
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            
             if (check(nums, k, mid)) {
-                // If true, it means we have ENOUGH pairs (>= k) with distance <= mid.
-                // mid is a valid candidate for "k-th smallest", but we want to see 
-                // if a smaller distance can also satisfy the "k-th" rank.
                 ans = mid;
                 hi = mid - 1;
             } else {
-                // If false, we have fewer than k pairs. 
-                // mid is too strict (too small). We need a larger distance to include more pairs.
                 lo = mid + 1;
             }
         }
-        
         return ans;
     }
 };
