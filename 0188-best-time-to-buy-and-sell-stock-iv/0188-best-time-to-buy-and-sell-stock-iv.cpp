8a111f1f085a77ba72aa12b3af7420b279ac9bc2
Apurba Kumar Show <156194221+IITApurba@users.noreply.github.com>
2026-05-20 22:48:19 +0530

    Time: 62 ms (6.73%), Space: 32.4 MB (6.33%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -7,28 +7,24 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        if (n == 0 || k == 0) return 0;
+        vector<vector<vector<int>>> memo(n, vector<vector<int>>(k + 1, vector<int>(2, -1)));
+        return dfs(0, k, 0, prices, memo);
+    }
+
+private:
+    int dfs(int i, int k, int holding, vector<int>& prices, vector<vector<vector<int>>>& memo) {
+        if (k == 0 || i == prices.size()) return 0;
+        if (memo[i][k][holding] != -1) return memo[i][k][holding];
         
-        if (k >= n / 2) {
-            int profit = 0;
-            for (int i = 1; i < n; i++) {
-                if (prices[i] > prices[i - 1]) {
-                    profit += prices[i] - prices[i - 1];
-                }
-            }
-            return profit;
+        int skip = dfs(i + 1, k, holding, prices, memo);
+        int take = 0;
+        
+        if (holding) {
+            take = prices[i] + dfs(i + 1, k - 1, 0, prices, memo);
+        } else {
+            take = -prices[i] + dfs(i + 1, k, 1, prices, memo);
         }
         
-        vector<int> buy(k + 1, -1e9);
-        vector<int> sell(k + 1, 0);
-        
-        for (int price : prices) {
-            for (int i = 1; i <= k; i++) {
-                buy[i] = max(buy[i], sell[i - 1] - price);
-                sell[i] = max(sell[i], buy[i] + price);
-            }
-        }
-        
-        return sell[k];
+        return memo[i][k][holding] = max(skip, take);
     }
 };
