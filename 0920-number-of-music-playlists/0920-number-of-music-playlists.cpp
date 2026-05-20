1abc1136952d1d8f80f868efe3f33224d864ff81
Apurba Kumar Show <156194221+IITApurba@users.noreply.github.com>
2026-05-20 22:51:13 +0530

    Time: 3 ms (50.92%), Space: 11.8 MB (48.34%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
@@ -3,21 +3,24 @@
 using namespace std;
 
 class Solution {
+    int mod = 1e9 + 7;
 public:
     int numMusicPlaylists(int n, int goal, int k) {
-        int mod = 1e9 + 7;
-        vector<vector<long long>> dp(goal + 1, vector<long long>(n + 1, 0));
-        dp[0][0] = 1;
+        vector<vector<long long>> dp(goal + 1, vector<long long>(n + 1, -1));
+        return rec(goal, n, n, k, dp);
+    }
+
+private:
+    long long rec(int i, int j, int n, int k, vector<vector<long long>>& dp) {
+        if (i == 0) return j == 0 ? 1 : 0;
+        if (j == 0) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
         
-        for (int i = 1; i <= goal; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = (dp[i - 1][j - 1] * (n - j + 1)) % mod;
-                if (j > k) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j] * (j - k)) % mod;
-                }
-            }
+        long long res = (rec(i - 1, j - 1, n, k, dp) * (n - (j - 1))) % mod;
+        if (j > k) {
+            res = (res + rec(i - 1, j, n, k, dp) * (j - k)) % mod;
         }
         
-        return dp[goal][n];
+        return dp[i][j] = res;
     }
 };
