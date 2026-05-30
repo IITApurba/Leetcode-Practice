bc6a8f29e174aba5ef234c9d64a2bfae62119500
Apurba Kumar Show <156194221+IITApurba@users.noreply.github.com>
2026-05-30 23:38:21 +0530

    Time: 29 ms (81.31%), Space: 8.8 MB (85.44%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+    bool isPerfectSquare(int n) {
+        if(n<=0)return false;
+        int root=sqrt(n);
+        return (root*root==n);
+    }
     bool winnerSquareGame(int n) {
-        vector<bool> dp(n + 1, false);
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j * j <= i; j++) {
-                if (!dp[i - j * j]) {
-                    dp[i] = true;
+        vector<bool> dp(n+1,false);
+        for(int i=1;i<=n;i++){
+            for(int k=1;k*k<=i;k++){
+                if(dp[i-k*k]==false){
+                    dp[i]=true;
                     break;
                 }
             }
         }
-
         return dp[n];
     }
 };
