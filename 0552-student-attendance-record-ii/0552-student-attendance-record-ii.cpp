b8e47a625daeaffdf0c2be1898e029d79012e4b6
Apurba Kumar Show <156194221+IITApurba@users.noreply.github.com>
2026-05-31 10:10:42 +0530

    Time: 1311 ms (38.73%), Space: 400.6 MB (36.83%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,20 +1,25 @@
 class Solution {
     int mod = 1e9 + 7;
-    
-    int solve(int n, int a, int l, vector<vector<vector<int>>>& memo) {
-        if (n == 0) return 1;
-        if (memo[n][a][l] != -1) return memo[n][a][l];
 
-        long long ans = solve(n - 1, a, 0, memo); 
-        if (a < 1) ans = (ans + solve(n - 1, a + 1, 0, memo)) % mod;
-        if (l < 2) ans = (ans + solve(n - 1, a, l + 1, memo)) % mod;
+    int solve(int n, int a, int l, vector<vector<vector<int>>>& dp) {
+        if (n == 0)
+            return 1;
+        if (dp[n][a][l] != -1)
+            return dp[n][a][l];
 
-        return memo[n][a][l] = ans;
+        long long ans = solve(n - 1, a, 0, dp);
+        if (a < 1)
+            ans = (ans + solve(n - 1, a + 1, 0, dp)) % mod;
+        if (l < 2)
+            ans = (ans + solve(n - 1, a, l + 1, dp)) % mod;
+
+        return dp[n][a][l] = ans;
     }
 
 public:
     int checkRecord(int n) {
-        vector<vector<vector<int>>> memo(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
-        return solve(n, 0, 0, memo);
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
+        return solve(n, 0, 0, dp);
     }
 };
